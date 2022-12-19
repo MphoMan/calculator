e90433d10395fc1fb23d4540007b9473ba748759
Mpho Manaka <mpho.manaka@jumpco.io>
2022-12-19 23:14:20 +0200

    fixing squared button

--- script.js
+++ script.js
@@ -8,15 +8,9 @@ try {
     }
     
     function calculations(){
-        if(id == "pwr"){
-            eval(Math.pow(document.getElementById('screen').value,2));
-            document.getElementById('screen').value= numbers;
-           }
-           else {
        var number = document.getElementById('screen').value;
        var numbers = eval(number);
        document.getElementById('screen').value= numbers;
-    }
        
     }
 } catch (error) {
